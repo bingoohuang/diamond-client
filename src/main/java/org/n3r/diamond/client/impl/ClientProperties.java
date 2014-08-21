414469822dbe171558848a1adcf2f92d38c87083
bingoohuang <bingoo.huang@gmail.com>
2014-08-21 16:03:20 +0800

    refactoring

--- src/main/java/org/n3r/diamond/client/impl/ClientProperties.java
+++ src/main/java/org/n3r/diamond/client/impl/ClientProperties.java
@@ -2,33 +2,17 @@ package org.n3r.diamond.client.impl;
 
 import com.google.common.base.Splitter;
 import com.google.common.net.HostAndPort;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
 import java.util.List;
 import java.util.Properties;
 
-import static java.io.File.separator;
-
 public class ClientProperties {
-    private static Logger log = LoggerFactory.getLogger(ClientProperties.class);
+    static Logger log = LoggerFactory.getLogger(ClientProperties.class);
 
-    private static Properties properties = new Properties();
-
-    static {
-        InputStream is = null;
-        try {
-            is = toInputStreamFromCdOrClasspath("diamond-client.properties", true);
-            if (is != null) properties.load(is);
-        } catch (IOException e) {
-            log.error("load properties error", e);
-        } finally {
-            IOUtils.closeQuietly(is);
-        }
-    }
+    static Properties properties = Props.tryProperties("diamond-client.properties", ".diamond-client");
 
     public static HostAndPort readNameServerAddress() {
         String nameServerAddress = properties.getProperty(Constants.NAME_SERVER_ADDRESS);
@@ -67,44 +51,4 @@ public class ClientProperties {
     public static enum NameServerMode {
         Off, ByEtcHosts, ByAddressProperty
     }
-
-    public static InputStream toInputStreamFromCdOrClasspath(String pathname, boolean silent) {
-        InputStream is = readFileFromCurrentDir(new File(pathname));
-        if (is != null) return is;
-
-        is = readFileFromDiamondClientHome(pathname);
-        if (is != null) return is;
-
-        is = getClassPathResourceAsStream(pathname);
-        if (is != null || silent) return is;
-
-        throw new RuntimeException("fail to find " + pathname + " in current dir or classpath");
-    }
-
-    private static InputStream readFileFromDiamondClientHome(String pathname) {
-        String filePath = System.getProperty("user.home") + separator + ".diamond-client";
-        File dir = new File(filePath);
-        if (!dir.exists()) return null;
-
-        File file = new File(dir, pathname);
-
-        return readFileFromCurrentDir(file);
-    }
-
-    private static InputStream readFileFromCurrentDir(File file) {
-        if (!file.exists()) return null;
-
-        try {
-            return new FileInputStream(file);
-        } catch (FileNotFoundException e) {
-            // This should not happened
-            log.error("read file {} error", file, e);
-            return null;
-        }
-    }
-
-    public static InputStream getClassPathResourceAsStream(String resourceName) {
-        return ClientProperties.class.getClassLoader().getResourceAsStream(resourceName);
-    }
-
 }
